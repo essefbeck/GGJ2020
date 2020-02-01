4aa99566aa302db7e4b7f8316c1ee09e6c2ffcc5
Grungsaw <60546852+Grungsaw@users.noreply.github.com>
2020-02-01 12:35:05 -0500

    New Boss-chan and background

--- Assets/Scripts/BossRotate.cs
+++ Assets/Scripts/BossRotate.cs
@@ -13,6 +13,6 @@ public class BossRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 50 * Time.deltaTime);
+        transform.Rotate(0, 0, -50 * Time.deltaTime);
     }
 }
