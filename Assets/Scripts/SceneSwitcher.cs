823bf6e84c4cf4c721a64853143217e0b5a59100
Grungsaw <60546852+Grungsaw@users.noreply.github.com>
2020-02-01 18:00:40 -0500

    Adding Scene Switcher

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Main");
+        //SceneManager.LoadScene("main");
+    }
+
+    public void GotoCreditsScene()
+    {
+        //SceneManager.LoadScene("menu");
+    }
+}
