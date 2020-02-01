37d7c5f32612bb4387c51908f44991cc953d87a8
David <moreoverlords@gmail.com>
2020-02-01 09:58:45 -0500

    scrolling stuff

---
+++ Assets/Scripts/ScrollManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollManager : MonoBehaviour
+{
+    public static ScrollManager Instance { get; private set; }
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = ScrollManager.Instance.GetSpeed();
+        rigidbody2d.velocity = new Vector2(-1 * speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Eraser")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
